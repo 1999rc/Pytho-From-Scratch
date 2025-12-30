704adb54673537c1279564e95f555fa3a6d1cecf
Raees chishty <mohommedraees@gmail.com>
2025-12-30 11:16:19 +0530

    Lesson 8:elif and multiple conditions

---
+++ lesson_8.py
@@ -0,0 +1,27 @@
+score =80 
+if score >=90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+elif score >= 50:
+    print("Grade C")
+else:
+    print("fail")
+
+"""
+Another:Example
+"""
+x=0.1
+if x >0:
+    print("Negative")
+elif x < 0:
+    print("Psitive")
+
+temperature=30 
+if temperature >35:
+    print("very hot")
+elif temperature > 25:
+    print("warm")
+else:
+    print("Cool")
+
