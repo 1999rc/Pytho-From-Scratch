176afa69a49bc435c34380b0419efe36c22bca22
Raees chishty <mohommedraees@gmail.com>
2025-12-30 10:29:01 +0530

    Lesson 4:printing variables with test

---
+++ lesson_4.py
@@ -0,0 +1,14 @@
+name = "Raees"
+print("My name is:",name)
+age = 25 
+print("My age:",age)
+age = 25 
+print("My age is:"+str(age))
+
+"""
+Another:Example
+"""
+name = "Raees"
+age = 25 
+print("Name",name)
+print("Age",age)
