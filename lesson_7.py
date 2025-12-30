0584fa27eafc22dc472e42a25717eb92ea4200b0
Raees chishty <mohommedraees@gmail.com>
2025-12-30 10:58:01 +0530

    Lesson 7:if and else decision making

---
+++ lesson_7.py
@@ -0,0 +1,13 @@
+age = 20 
+if age > 18:
+    print("you are adult")
+
+"""
+Another:Example
+"""
+age1 = 15 
+if age1 > 18:
+    print("you are an adult")
+else:
+    print("you are not an Adult")
+    
